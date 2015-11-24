ca706070fa57551eb26371a8a083690eb49cc949
Frantisek Rezac <frantisek.rezac@calavera.info>
2015-11-24 20:54:14 +0100

    Refactoring for conceptual purity

--- src/main/resources/public/js/task-list.js
+++ src/main/resources/public/js/task-list.js
@@ -3,10 +3,13 @@ taskListModel.newTask = {};
 taskListModel.newTask.formSubmissions = new Rx.Subject();
 taskListModel.task = {};
 taskListModel.task.updates = new Rx.Subject();
+taskListModel.task.deletes = new Rx.Subject();
 
-taskListModel.task.updateConfirmations = taskListModel.task.updates
-	.flatMap(taskUpdate => Rx.Observable.fromPromise(
-		taskUpdate.isDelete ? common.doDelete(taskUpdate.url) : common.doPut(taskUpdate.url, taskUpdate.entity)));
+taskListModel.task.updateConfirmations = Rx.Observable.merge(
+	taskListModel.task.updates
+		.flatMap(taskUpdate => Rx.Observable.fromPromise(common.doPut(taskUpdate.url, taskUpdate.entity))),
+	taskListModel.task.deletes
+		.flatMap(deleteUrl => Rx.Observable.fromPromise(common.doDelete(deleteUrl))));
 
 taskListModel.newTask.confirmations = taskListModel.newTask.formSubmissions
 	.withLatestFrom(
@@ -44,32 +47,36 @@ var Task = React.createClass({
 			completed: this.props.data.completed
 		};
 	},
-	onSave: function(isDelete) {
+	onSave: function(e) {
+		e.preventDefault();
 		this.setState({editable: false});
 		this.props.onSubmit({
-			entity: {
-				description: this.state.description,
-				completed: this.state.completed
-			},
-			url: this.props.data._links.self.href,
-			isDelete: isDelete
-		});
+			description: this.state.description,
+			completed: this.state.completed
+		}, this.props.data._links.self.href);
+	},
+	onDelete: function(e) {
+		this.props.onDelete(this.props.data._links.self.href);
+	},
+	enableEdit: function() {this.setState({editable: true});},
+	disableEdit: function() {
+		this.setState(this.getInitialState());
 	},
 	renderReadOnly: function() {
 		return (
 			<form>
-				<input checked={this.state.completed} onChange={e => {this.setState({completed: e.target.value}); this.onSave(false)}} type="checkbox" />
-				<span onClick={() => {this.setState({editable: true});}}>{this.props.data.description}</span>
+				<input checked={this.state.completed} onChange={e => {this.setState({completed: e.target.value}); this.onSave(e);}} type="checkbox" />
+				<span onClick={this.enableEdit}>{this.props.data.description}</span>
 			</form>
 		);
 	},
 	renderEditable: function() {
 		return (
-			<form onSubmit={e => {e.preventDefault(); this.onSave(false);}}>
+			<form onSubmit={this.onSave}>
 				<input value={this.state.description} onChange={e => this.setState({description: e.target.value})} type="text" />
 				<input value="Save" type="submit" />
-				<input value="Delete" onClick={() => {this.onSave(true);}} type="button" />
-				<input value="Cancel" onClick={() => {this.setState(this.getInitialState());}} type="button" />
+				<input value="Delete" onClick={this.onDelete} type="button" />
+				<input value="Cancel" onClick={this.disableEdit} type="button" />
 			</form>
 		);
 	},
@@ -91,7 +98,7 @@ var TaskList = React.createClass({
 				<h1>{this.state.taskList.title}</h1>
 				<ul>{this.state.tasks.map(task =>
 					<li key={task._links.self.href}>
-						<Task data={task} onSubmit={this.props.onSubmit} />
+						<Task data={task} onSubmit={this.props.onSubmit} onDelete={this.props.onDelete} />
 					</li>
 				)}</ul>
 			</div>
@@ -107,15 +114,18 @@ var TaskListController = React.createClass({
 		task.taskList = this.taskListComponent.state.taskList._links.self.href;
 		this.props.model.newTask.formSubmissions.onNext(task);
 	},
-	updateTask: function(task) {
-		this.props.model.task.updates.onNext(task);
+	updateTask: function(task, id) {
+		this.props.model.task.updates.onNext({entity: task, url: id});
+	},
+	deleteTask: function(id) {
+		this.props.model.task.deletes.onNext(id)
 	},
 	render: function() {
 		return (
 			<div>
 				<common.Menu />
 				<NewTask onSubmit={this.newTask} ref={newTask => {this.newTaskComponent = newTask}}/>
-				<TaskList onSubmit={this.updateTask} ref={taskList => {this.taskListComponent = taskList;}}/>
+				<TaskList onSubmit={this.updateTask} onDelete={this.deleteTask} ref={taskList => {this.taskListComponent = taskList;}}/>
 			</div>
 		);
 	}
